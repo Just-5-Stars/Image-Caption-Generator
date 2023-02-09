57fbb7c905f936ee8ccb0d328cdee919f9907ef0
Aditya <adityarags@gmail.com>
2023-02-10 00:46:22 +0530

    Application Setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from application.config import Config
+
+app = Flask(__name__, template_folder = "templates")
+app.config.from_object(Config)
+app.app_context().push()
+
+if __name__ == "__main__":
+    from application.controllers import *
+    app.run()
