8d6f665a99ea2effe00af453b2e4492362bacbaa
Aditya <adityarags@gmail.com>
2023-02-11 17:31:42 +0530

    Deployment: Flask Code Ready

--- application/controllers.py
+++ application/controllers.py
@@ -1,12 +1,13 @@
 from flask import current_app as app, request, render_template
 import os
-from model.prediction import predict_caption
+from model.prediction import predict
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
+    caption_pred = ""
     if request.method == "POST":
         f = request.files["image"]
         path = os.path.join(app.config['UPLOAD_FOLDER'], "inputImage.jpg")
         f.save(path)
-        print(predict_caption())
-    return render_template("index.html")
+        caption_pred = predict()
+    return render_template("index.html", caption = caption_pred)
--- model/prediction.py
+++ model/prediction.py
@@ -1,15 +1,57 @@
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+import numpy as np
+import pickle
 
+with open("miscellaneous//cleanedCaption.pickle", "rb") as f:
+    caption_mapping = pickle.load(f)
+
+all_captions = []
+for key in caption_mapping:
+    for caption in caption_mapping[key]:
+        all_captions.append(caption)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(all_captions)
+vocab_size = len(tokenizer.word_index) + 1
+
+max_length = max(len(caption.split()) for caption in all_captions)
 
 vgg_model = VGG16() 
 
 vgg_model = Model(inputs=vgg_model.inputs,             
                   outputs=vgg_model.layers[-2].output)
 
-def predict_caption():
-    image_path = '/app_data/inputImage.jpg'
+model = tf.keras.models.load_model("model//backup_model30k.h5")
+
+def idx_to_word(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+def predict_caption(model, image, tokenizer, max_length):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], max_length)
+        yhat = model.predict([image, sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = idx_to_word(yhat, tokenizer)
+        if word is None:
+            break
+        in_text += " " + word
+        if word == 'endseq':
+            break
+    return in_text
+
+
+def predict():
+    image_path = 'app_data/inputImage.jpg'
     image = load_img(image_path, target_size=(224, 224))
     image = img_to_array(image)
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
